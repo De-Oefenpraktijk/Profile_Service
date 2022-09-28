f5b33cdd2c19b5f2cc51135a4afc41781832c7a1
Jop <j.bertrams@home.nl>
2022-09-28 11:44:29 +0200

    Added project from previous repository

---
+++ Profile_Service/Entities/DBContext.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Profile_Service.Models;
+
+
+namespace Profile_Service.Entities
+{
+    public partial class DBContext : DbContext
+    {
+        public DbSet<User> Users { get; set; }
+
+        public DBContext(DbContextOptions<DBContext> options) : base(options) { }
+    }
+}
---
+++ Profile_Service/Models/User.cs
@@ -0,0 +1,35 @@
+﻿using System.ComponentModel.DataAnnotations.Schema;
+using System.ComponentModel.DataAnnotations;
+
+namespace Profile_Service.Models
+{
+    public partial class User
+    {
+        [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public int Id { get; set; }
+
+        [Required]
+        public string? FirstName { get; set; }
+
+        [Required]
+        public string? LastName { get; set; }
+
+        [Required]
+        public string? Username { get; set; }
+
+        [Required]
+        [DataType(DataType.EmailAddress)]
+        public string? EmailAddress { get; set; }
+
+        [Required]
+        [DataType(DataType.Password)]
+        public string? Password { get; set; }
+
+        [Required]
+        public DateTime EnrollmentDate { get; set; }
+
+        [Required]
+        public string? Role { get; set; }
+    }
+}
