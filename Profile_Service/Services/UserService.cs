14a44b676dda82c42a462e9dfc940ae2576020e0
Jop <j.bertrams@home.nl>
2022-12-19 12:19:20 +0100

    Changed specializations and educations array

--- Profile_Service/Services/UserService.cs
+++ Profile_Service/Services/UserService.cs
@@ -29,25 +29,7 @@ namespace Profile_Service.Services
             var user = _mapper.Map<User>(_user);
             user.Role = "User";
 
-            var educationList = new List<string>();
-            var specializationList = new List<string>();
-
             await _context.Users.InsertOneAsync(user);
-
-            foreach (var education in user.Educations)
-            {
-                var educationName = await _context.Education.Find(x => x.Id == education).FirstOrDefaultAsync();
-                educationList.Add(educationName.Name);
-            }
-
-            foreach (var specialization in user.Specializations)
-            {
-                var specializationName = await _context.Specialization.Find(x => x.Id == specialization).FirstOrDefaultAsync();
-                specializationList.Add(specializationName.Name);
-            }
-
-            user.Specializations = specializationList;
-            user.Educations = educationList;
 
             var result = _mapper.Map<UserDTO>(user);
 
@@ -69,29 +51,12 @@ namespace Profile_Service.Services
         public async Task<UserDTO> GetUserByID(string userId)
         {
             var objectId = ObjectId.Parse(userId);
-            var educationList = new List<string>();
-            var specializationList = new List<string>();
             
             var user = await _context.Users.Find(x => x.Id == userId).FirstOrDefaultAsync();
             if (user == null)
             {
                 throw new Exception("User does not exist");
             }
-
-            foreach (var education in user.Educations)
-            {
-                var educationName = await _context.Education.Find(x => x.Id == education).FirstOrDefaultAsync();
-                educationList.Add(educationName.Name);
-            }
-
-            foreach (var specialization in user.Specializations)
-            {
-                var specializationName = await _context.Specialization.Find(x => x.Id == specialization).FirstOrDefaultAsync();
-                specializationList.Add(specializationName.Name);
-            }
-
-            user.Specializations = specializationList;
-            user.Educations = educationList;
 
             return _mapper.Map<UserDTO>(user);
         }
@@ -102,25 +67,7 @@ namespace Profile_Service.Services
             var user = _mapper.Map<User>(_user);
             user.Id = Id;
 
-            var educationList = new List<string>();
-            var specializationList = new List<string>();
-
             await _context.Users.ReplaceOneAsync(x => x.Id == Id, user);
-
-            foreach (var education in user.Educations)
-            {
-                var educationName = await _context.Education.Find(x => x.Id == education).FirstOrDefaultAsync();
-                educationList.Add(educationName.Name);
-            }
-
-            foreach (var specialization in user.Specializations)
-            {
-                var specializationName = await _context.Specialization.Find(x => x.Id == specialization).FirstOrDefaultAsync();
-                specializationList.Add(specializationName.Name);
-            }
-
-            user.Specializations = specializationList;
-            user.Educations = educationList;
 
             var result = _mapper.Map<UserDTO>(user);
 
